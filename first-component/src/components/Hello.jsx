ea6e0980231017f644f7bd74d81f400410e9d05a
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-05-20 14:26:24 +0530

    Component Using Without JSX

--- first-component/src/components/Hello.jsx
+++ first-component/src/components/Hello.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function Hello() {
-     return (
-          <div>
-               <h1>Hello Users</h1>
-          </div>
+     // return (
+     //      <div>
+     //           <h1>Hello Users</h1>
+     //      </div>
+     // );
+     return React.createElement(
+          "div",
+          {},
+          React.createElement("h1", {}, "Hello Users")
      );
 }
