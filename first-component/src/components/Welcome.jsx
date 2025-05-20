e6fa8418886cda67df3108f60f3df1f53b47b81a
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-05-20 14:00:26 +0530

    Class Component

---
+++ first-component/src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+class Welcome extends React.Component {
+     render() {
+          return (
+               <>
+                    <h1>Hello From Components/Welcome</h1>
+               </>
+          );
+     }
+}
+export default Welcome
