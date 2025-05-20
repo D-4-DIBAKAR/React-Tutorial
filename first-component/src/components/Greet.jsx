6da195a05bbc9f719d813838cc68637a62f93353
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-05-20 13:53:46 +0530

    My First Components

---
+++ first-component/src/components/Greet.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// export default function Greet() {
+//      return (
+
+//           <h1>Hello Your Name</h1>
+
+//      )
+// }
+
+const Greet = () => {
+     return (
+          <h1>Hello Your Name</h1>
+     )
+}
+
+export default Greet
