86b14449f5e4da9f74a9cde02a7a35ceaf7b7268
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-05-25 15:35:51 +0530

    Conditional Rendering

--- first-component/src/components/UserGreeting.jsx
+++ first-component/src/components/UserGreeting.jsx
@@ -9,6 +9,28 @@ class UserGreeting extends Component {
                isLoggedIn: false,
           }
      } render() {
+          //let message;
+          // if(this.state.isLoggedIn) {
+          //      message = <div>Welcome Rahul</div>
+          // } else {
+          //      message = <div>Welcome Guest</div>           
+          // }
+
+
+
+          // if(this.state.isLoggedIn) {
+          //      return (
+          //           <div>
+          //                <div>Welcome Rahul</div>
+          //           </div>
+          //      )
+          // } else {
+          //      return (
+          //           <div>
+          //                <div>Welcome Guest</div>
+          //           </div>
+          //      )
+          // }
           return (
                <div>
                     <div>Welcome {this.state.isLoggedIn ? "Rahul" : "Guest"}</div>
