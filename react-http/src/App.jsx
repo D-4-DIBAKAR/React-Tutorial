72d600f41434dcab1d6cee44a8811d4bba893361
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-05-29 15:12:18 +0530

    HTTP - POST Request

--- react-http/src/App.jsx
+++ react-http/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import PostForm from './components/PostForm'
 import PostList from './components/PostList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <PostList />
+      {/* <PostList /> */}
+      <PostForm />
     </>
   )
 }
