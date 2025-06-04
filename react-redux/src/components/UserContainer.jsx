b8f45232507a0a9704268f54db03139cf92cebd8
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-04 15:36:24 +0530

    redux basics with react

--- react-redux/src/components/UserContainer.jsx
+++ react-redux/src/components/UserContainer.jsx
@@ -8,7 +8,7 @@ function UserContainer({ userData, fetchUsers }) {
      console.log(userData);
 
      return (
-          <div>
+          <div className="user-container">
                {
                     userData.loading ? (
                          <h2>Loading ...</h2>
@@ -17,11 +17,32 @@ function UserContainer({ userData, fetchUsers }) {
                     ) : (
                          <div>
                               <h2>User List</h2>
-                              <div>
+                              <div className="user-list">
                                    {
-                                        userData.users && userData.users.map(user =>
-                                             <p key={user.id}>{user.name}</p>
-                                        )
+                                        userData.users && userData.users.map(user => (
+                                             <div className="user-card" key={user.id}>
+                                                  <h3>{user.name}</h3>
+                                                  <p><strong>Username:</strong> {user.username}</p>
+                                                  <p><strong>Email:</strong> {user.email}</p>
+                                                  <p><strong>Phone:</strong> {user.phone}</p>
+                                                  <p><strong>Website:</strong> <a href={`http://${user.website}`} target="_blank" rel="noreferrer">{user.website}</a></p>
+
+                                                  <div className="section">
+                                                       <h4>Address</h4>
+                                                       <p>{user.address.suite}, {user.address.street}</p>
+                                                       <p>{user.address.city} - {user.address.zipcode}</p>
+                                                       <p><strong>Geo:</strong> {user.address.geo.lat}, {user.address.geo.lng}</p>
+                                                  </div>
+
+                                                  <div className="section">
+                                                       <h4>Company</h4>
+                                                       <p><strong>Name:</strong> {user.company.name}</p>
+                                                       <p><strong>Catch Phrase:</strong> {user.company.catchPhrase}</p>
+                                                       <p><strong>Business:</strong> {user.company.bs}</p>
+                                                  </div>
+                                                  <hr />
+                                             </div>
+                                        ))
                                    }
                               </div>
                          </div>
