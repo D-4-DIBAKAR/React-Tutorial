46ef8c569307ffd4f89a18532a424bd3330def00
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-05 20:52:26 +0530

    redux basics  implementation with react

--- react-redux/src/components/UserContainer.jsx
+++ react-redux/src/components/UserContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../redux/user/userActions'
 function UserContainer({ userData, fetchUsers }) {
