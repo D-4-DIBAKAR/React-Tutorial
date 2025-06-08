9ece611962e21409d49047bc37ee98cb5cc84a49
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-08 13:08:17 +0530

    redux basics implemention

--- react-redux/src/redux-toolkit-basic/redux-rtk01.js
+++ react-redux/src/redux-toolkit-basic/redux-rtk01.js
@@ -1,11 +1,14 @@
 import store from "./app/store.js";
-import { ordered, restocked } from "./features/cake/cakeSlice.js";
-import { ordered as orderedIceCream, restocked as restockedIceCream } from "./features/iceCream/iceCreamSlice.js";
+// import { ordered, restocked } from "./features/cake/cakeSlice.js";
+// import { ordered as orderedIceCream, restocked as restockedIceCream } from "./features/iceCream/iceCreamSlice.js";
 import { fetchUsers } from "./features/user/userSlice.js";
 
 console.log("Initial state", store.getState());
 
-const unsubscribe = store.subscribe(() => {
+// const unsubscribe = store.subscribe(() => {
+//      console.log("Updated state", store.getState());
+// })
+store.subscribe(() => {
      console.log("Updated state", store.getState());
 })
 
