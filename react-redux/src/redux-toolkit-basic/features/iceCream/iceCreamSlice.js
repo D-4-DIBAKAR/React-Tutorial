d14d3f47c994ba3dac4af3b3e382a37c08f0cb52
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-08 13:06:30 +0530

    redux basics implemention

--- react-redux/src/redux-toolkit-basic/features/iceCream/iceCreamSlice.js
+++ react-redux/src/redux-toolkit-basic/features/iceCream/iceCreamSlice.js
@@ -8,8 +8,41 @@ const iceCreamSlice = createSlice({
      reducers: {
           ordered: (state) => { state.numOfIceCreams-- },
           restocked: (state, action) => { state.numOfIceCreams += action.payload }
+     },
+     extraReducers: (builder) => {
+          builder.addCase('cake/ordered', (state) => {
+               state.numOfIceCreams--
+          })
      }
 })
 
-export default iceCreamSlice.reducer; //FIX: use reducer, not reducers
-export const { ordered, restocked } = iceCreamSlice.actions
+export default iceCreamSlice.reducer;
+export const { ordered, restocked } = iceCreamSlice.actions;
+
+
+
+// import { createSlice } from '@reduxjs/toolkit'
+
+// const initialState = { numOfIceCreams: 20 }
+
+// const iceCreamSlice = createSlice({
+//      name: 'icecream',
+//      initialState,
+//      reducers: {
+//           ordered: (state) => { state.numOfIceCreams-- },
+//           restocked: (state, action) => { state.numOfIceCreams += action.payload }
+//      },
+//      // extraReducers: (builder) => {
+//      //      builder.addCase('cake/ordered', (state) => {
+//      //           state.numOfIceCreams--
+//      //      })
+//      // },
+//      extraReducers: {
+//           ['cake/ordered']: (state) => {
+//                state.numOfIceCreams--
+//           }
+//      }
+// })
+
+// export default iceCreamSlice.reducer;
+// export const { ordered, restocked } = iceCreamSlice.actions
--- react-redux/src/redux-toolkit-basic/redux-rtk01.js
+++ react-redux/src/redux-toolkit-basic/redux-rtk01.js
@@ -1,6 +1,7 @@
 import store from "./app/store.js";
 import { ordered, restocked } from "./features/cake/cakeSlice.js";
 import { ordered as orderedIceCream, restocked as restockedIceCream } from "./features/iceCream/iceCreamSlice.js";
+import { fetchUsers } from "./features/user/userSlice.js";
 
 console.log("Initial state", store.getState());
 
@@ -8,13 +9,15 @@ const unsubscribe = store.subscribe(() => {
      console.log("Updated state", store.getState());
 })
 
-store.dispatch(ordered());
-store.dispatch(ordered());
-store.dispatch(ordered());
-store.dispatch(restocked(3));
+// store.dispatch(ordered());
+// store.dispatch(ordered());
+// store.dispatch(ordered());
+// store.dispatch(restocked(3));
 
-store.dispatch(orderedIceCream());
-store.dispatch(orderedIceCream());
-store.dispatch(restockedIceCream(2));
+// store.dispatch(orderedIceCream());
+// store.dispatch(orderedIceCream());
+// store.dispatch(restockedIceCream(2));
 
-unsubscribe();
+store.dispatch(fetchUsers());
+
+// unsubscribe();
