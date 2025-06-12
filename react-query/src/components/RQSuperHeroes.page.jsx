3f462d1de4009f299612757037ae02e0f32d9af9
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-12 16:33:56 +0530

    Success or Error CallBack

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -3,6 +3,12 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes')
 export function RQSuperHeroesPage() {
+     const onSuccess = (data) => {
+          console.log('perform side effect after data fetching', data);
+     }
+     const onError = (error) => {
+          console.log('perform side effect after encountering error', error);
+     }
      // const { isLoading, error, data, isError, isFetching } = useQuery('super-heroes', fetchSuperHeroes)
      const { isLoading, error, data, isError, isFetching, refetch } = useQuery('super-heroes', fetchSuperHeroes, {
           // cacheTime: 5000,
@@ -12,13 +18,15 @@ export function RQSuperHeroesPage() {
           // refetchOnWindowFocus: false, //default is true, or another value can be passed 'always',
           // refetchInterval: 1000,//default is false, or another value can be passed in miliseconds, //it stops fetching data when window is not in focus
           // refetchIntervalInBackground: false, //default is false, or another value can be passed in miliseconds //it continue fetching data when window is not in focus,
-          enabled: false //default is true used for disable the query on mount , now we can use button to trigger the query
+          // enabled: false //default is true used for disable the query on mount , now we can use button to trigger the query
+          onSuccess,
+          onError
 
 
 
 
      })
-     console.log(isLoading, isFetching);
+     // console.log(isLoading, isFetching);
 
      if (isLoading) {
           return <h2>Loading...</h2>
@@ -30,10 +38,11 @@ export function RQSuperHeroesPage() {
      if (isError) {
           return <h2>{error.message}</h2>
      }
+
      return (
           <div>
                <h2>RQ Super Heroes Page</h2>
-               <button onClick={refetch}>Fetch Heroes</button>
+               {/* <button onClick={refetch}>Fetch Heroes</button> */}
                {data?.data.map(hero => {
                     return <div key={hero.id}>{hero.name}</div>
                })}
