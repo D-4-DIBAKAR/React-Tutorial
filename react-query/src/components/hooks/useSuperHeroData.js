5de45b250c3e30ca3ea3b665cf1e3feb464b4ac0
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-12 21:33:35 +0530

    Initial Data

--- react-query/src/components/hooks/useSuperHeroData.js
+++ react-query/src/components/hooks/useSuperHeroData.js
@@ -1,8 +1,17 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 const fetchSuperHero = ({ queryKey }) => {
      const heroId = queryKey[1];
      return axios.get(`http://localhost:4000/superheroes/${heroId}`)
 };
 // export const useSuperHeroData = (heroId) => useQuery(['super-hero', heroId], () => fetchSuperHero(heroId));
-export const useSuperHeroData = (heroId) => useQuery(['super-hero', heroId], fetchSuperHero);
+export const useSuperHeroData = (heroId) => {
+     const queryClient = useQueryClient();
+     return useQuery(['super-hero', heroId], fetchSuperHero, {
+          initialData: () => {
+               const hero = queryClient.getQueryData('super-heroes')?.data.find(hero => hero.id === parseInt(heroId))
+               if (hero) return { data: hero }
+
+          },
+     })
+};
