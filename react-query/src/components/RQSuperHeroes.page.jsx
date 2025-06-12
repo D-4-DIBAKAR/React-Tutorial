f7916017de274372f915b8728971978eb72dc31f
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-12 15:58:27 +0530

    Query Cache

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -3,15 +3,15 @@ import { useQuery } from 'react-query'
 import axios from 'axios'
 const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes')
 export function RQSuperHeroesPage() {
-     const { isLoading, error, data, isError, isFetching } = useQuery('super-heroes', fetchSuperHeroes)
+     const { isLoading, error, data, isError, isFetching } = useQuery('super-heroes', fetchSuperHeroes, { cacheTime: 5000 })
      console.log(isLoading, isFetching);
 
      if (isLoading) {
           return <h2>Loading...</h2>
      }
-     if (isFetching) {
-          return <h2>Fetching...</h2>
-     }
+     // if (isFetching) {
+     //      return <h2>Fetching...</h2>
+     // }
      if (isError) {
           return <h2>{error.message}</h2>
      }
