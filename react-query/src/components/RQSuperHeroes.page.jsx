07ba85d585eb5a035097b198db2eb97aac5a82fc
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-12 15:29:39 +0530

    Fetching Data with useQuery

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import { useQuery } from 'react-query'
+import axios from 'axios'
+const fetchSuperHeroes = () => axios.get('http://localhost:4000/superheroes')
 export function RQSuperHeroesPage() {
+     const { isLoading, error, data } = useQuery('super-heroes', fetchSuperHeroes)
+
+     if (isLoading) {
+          return <h2>Loading...</h2>
+     }
+     if (error) {
+          return <h2>{error.message}</h2>
+     }
      return (
-          <div>RQSuperHeroesPage</div>
+          <div>
+               <h2>RQ Super Heroes Page</h2>
+               {data?.data.map(hero => {
+                    return <div key={hero.id}>{hero.name}</div>
+               })}
+          </div>
      )
 }
 
