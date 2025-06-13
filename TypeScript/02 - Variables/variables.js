cb434e4f9d9566dc51770166ed1d158bbbbdd396
DIBAKAR PARIDA <dibakarparida586@gmail.com>
2025-06-14 01:32:03 +0530

    Revert "Reapply "Merge pull request #1 from D-4-DIBAKAR/TypeScript""
    This reverts commit 81ad8dc8eb4a8fe42f70b7090090eabd202deddf.

--- TypeScript/02 - Variables/variables.js
+++
@@ -1,7 +0,0 @@
-/**Variables in TypeScript */
-//variable declared with 'let' can be assigned later time also update any point of time
-var num;
-num = 100;
-num = 1000;
-//variable declared with 'const' can not be assigned later time must be assigned during declaration also can not update any point of time
-var str = "Hello World!";
