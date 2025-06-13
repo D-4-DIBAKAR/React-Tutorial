1705daa55baebdd67900a8c6d5f6d0fb2297cb8d
DIBAKAR PARIDA <71878062+D-4-DIBAKAR@users.noreply.github.com>
2025-06-14 01:50:55 +0530

    Revert "Type script"

---
+++ TypeScript/02 - Variables/variables.ts
@@ -0,0 +1,6 @@
+/**Variables in TypeScript */
+
+let num = 100;
+num = 1000;
+
+const str = "Hello World!";
---
+++ TypeScript/03 - Datatypes in TypeScript/dataTypes.js
@@ -0,0 +1,16 @@
+//String
+var str1 = "This is a string created using single quote";
+var str2 = "This is a string created using double quote";
+var str3 = "This is a string created using back ticks ".concat(str1, " \nthis is another line");
+console.log(str1);
+console.log(str3);
+//Number (Integer , Floating Point or any other types Number)
+var num = 12;
+var PI = 3.14;
+//Boolean [true ,false]
+var isEligible = true;
+var isEqual = false;
+//truthy => 1," ",true
+//falsy => 0,"",false,null,undefined etc
+var isGreater = 10 > 15;
+console.log(isGreater);
